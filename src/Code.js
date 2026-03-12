c4e04be10db26d419d037444069f1baec477e034
Nonthapat Jamjai <60281096+Deknoinarak@users.noreply.github.com>
2026-03-12 09:40:32 +0700

    Delete src directory

--- src/Code.js
+++
@@ -1,8 +0,0 @@
-function doGet(e) {
-  const template = HtmlService.createTemplateFromFile("Index");
-
-  return template
-    .evaluate()
-    .setTitle("ระบบประเมินผลผผู้ปฏิบัติงาน KNSchool")
-    .addMetaTag("viewport", "width=device-width, initial-scale=1");
-}
